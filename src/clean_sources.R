cac64399b2f1811be54accd3d4ad3188fac95840
bestxolodec <1xolodec@gmail.com>
2017-04-17 15:23:34 +0300

    removed empty trainling spaces, removed inclusion of headers

--- src/clean_sources.R
+++ src/clean_sources.R
@@ -1,14 +1,14 @@
 
-source("~/yandexDisk/DIPLOMA//OWL/O_learning_functions.r")
+#source("~/yandexDisk/DIPLOMA//OWL/O_learning_functions.r")
 
 library("truncnorm")
 
 rtruncnorm <- function (n, a = -Inf, b = Inf, mean = 0, sd = 1)  {
-  if (length(n) > 1) 
+  if (length(n) > 1)
     n <- length(n)
-  if (length(n) > 1) 
+  if (length(n) > 1)
     n <- length(n)
-  else if (!is.numeric(n)) 
+  else if (!is.numeric(n))
     stop("non-numeric argument n.")
   .Call("do_rtruncnorm", as.integer(n), a, b, mean, sd, PACKAGE="truncnorm")
 }
@@ -19,7 +19,7 @@ GetRewardGivenQfunctionValuesAsMeanVec <- function(q.function.values, sd=1,
                                                    only.positive=TRUE, eps=0.01) {
   raw.reward <- rnorm(NROW(q.function.values), q.function.values, sd)
   reward <-  raw.reward - min(raw.reward) + eps
-  return (list(raw.reward=as.matrix(raw.reward), 
+  return (list(raw.reward=as.matrix(raw.reward),
                reward=as.matrix(reward)))
 }
 
@@ -38,7 +38,7 @@ ReplaceExtremeWithUnif <- function(values, min.val, max.val){
 
 Shvechikov.1.fopt <- function(x) {
   return (((x - .5) / .5) ** 2)
-} 
+}
 
 
 GetDataForShvechikov.1 <- function(sample.size,  sd, min.A=0, max.A=1, min.X=0, max.X=1){
@@ -53,7 +53,7 @@ GetDataForShvechikov.1 <- function(sample.size,  sd, min.A=0, max.A=1, min.X=0, 
   A.opt <- Shvechikov.1.fopt(X)
   Q.vals <-GetQFunctionValues(X, A, A.opt)
   R.list <- GetRewardGivenQfunctionValuesAsMeanVec(Q.vals, sd = sd)
-  data <- list(covariates = X, treatment=A, optimal.treatment=A.opt, 
+  data <- list(covariates = X, treatment=A, optimal.treatment=A.opt,
                prop.scores = rep(1, sample.size))
   data$GetQFunctionValues = GetQFunctionValues
   data <- c(data, R.list)
@@ -68,15 +68,15 @@ Shvechikov.2.fopt <- function(x) {
   step.start = 15
   breakage.start = 40
   first.part <- x**2  *  exp(- x**(.8)) +  (x - step.start) / 50 + .5
-  first.and.changed.part <- ( - x**1.9 / 900 + 6 *  sin(x/6) + 1.5 * (sin(x**1.01 ) + 1) * x**(1/7) )* 1/20 
+  first.and.changed.part <- ( - x**1.9 / 900 + 6 *  sin(x/6) + 1.5 * (sin(x**1.01 ) + 1) * x**(1/7) )* 1/20
   second.part <-  (- first.part + first.and.changed.part) * (x > breakage.start)
   whole <- first.part + second.part
   f <- function(x) { # scaled Runge function
     x <- x / 60   - 0.3
-    cos(3*pi*x)/(1+25*(x-0.25)^2) 
+    cos(3*pi*x)/(1+25*(x-0.25)^2)
   }
   return ((whole + f(x)) / 2 + 0.35)
-} 
+}
 
 
 GetDataForShvechikov.2 <- function(sample.size, sd, min.A=0, max.A=1, min.X=0, max.X=100) {
@@ -86,13 +86,13 @@ GetDataForShvechikov.2 <- function(sample.size, sd, min.A=0, max.A=1, min.X=0, m
     }
     return (-(given.A - optimal.A) ** 2)
   }
-  X <- ReplaceExtremeWithUnif(rgamma(n=sample.size,  shape=13, rate=0.3), min.X, max.X) 
+  X <- ReplaceExtremeWithUnif(rgamma(n=sample.size,  shape=13, rate=0.3), min.X, max.X)
   X <- X / 100 # hack to bring X in [0,1] range
   A <- ReplaceExtremeWithUnif(rgamma(n=sample.size,  shape=2, rate=4.21), min.A, max.A)
   A.opt <-  Shvechikov.2.fopt(X)
   Q.vals <-GetQFunctionValues(X, A, A.opt)
   R.list <- GetRewardGivenQfunctionValuesAsMeanVec(Q.vals, sd = sd)
-  data <- list(covariates = X, treatment=A, optimal.treatment=A.opt, 
+  data <- list(covariates = X, treatment=A, optimal.treatment=A.opt,
                prop.scores = rep(1, sample.size))
   data$GetQFunctionValues = GetQFunctionValues
   data <- c(data, R.list)
@@ -108,16 +108,16 @@ GetDataForShvechikov.2 <- function(sample.size, sd, min.A=0, max.A=1, min.X=0, m
 # Wrappers to allow using Chen code ---------------------------------------
 
 ChangeFormatFromOurToChenEnriched <- function(our_data) {
-  with(our_data, list(X=covariates, A=treatment, R=raw.reward, D_opt=optimal.treatment, 
+  with(our_data, list(X=covariates, A=treatment, R=raw.reward, D_opt=optimal.treatment,
                       mu=GetQFunctionValues(covariates, treatment, optimal.treatment),
-                      GetQFunctionValues=GetQFunctionValues, 
+                      GetQFunctionValues=GetQFunctionValues,
                       GetOptimalTreatment=GetOptimalTreatment))
 }
 
 
 ChangeFormatFromChenEnrichedToOur <- function(chen_data, eps=0.01) {
-  with(chen_data, list(covariates=X, treatment=A, raw.reward=R, 
-                       optimal.treatment=D_opt, reward=R - min(R) + eps, 
+  with(chen_data, list(covariates=X, treatment=A, raw.reward=R,
+                       optimal.treatment=D_opt, reward=R - min(R) + eps,
                        GetQFunctionValues=GetQFunctionValues,
                        GetOptimalTreatment=GetOptimalTreatment))
 }
@@ -136,7 +136,7 @@ PredValueGeneral <- function(A_pred, test) {
 
 # slightly rewritten function from KO-learning to allow code reuse
 pred_ko <- function(model, test) {
-  # if (any((o > 2) | (o < 0))) 
+  # if (any((o > 2) | (o < 0)))
   warning("Clipping predicted treatment to [0,2]!")
   A_pred <- pmin(pmax(predict(model,test$X), 0), 2)
   return(list(A_pred=A_pred, Q=PredValueGeneral(A_pred, test)))
@@ -145,7 +145,7 @@ pred_ko <- function(model, test) {
 GetKOLearningValueAndPredictedDose <- function(train, test, q = 0.6) {
   train$weight = with(train, R - min(quantile(R, q),0))
   index = with(train, which(R > quantile(R,q)))
-  model = with(train, svm(x = X[index,], y = A[index], w=weight[index],   
+  model = with(train, svm(x = X[index,], y = A[index], w=weight[index],
                           type="eps-regression", epsilon = 0.15, scale=FALSE))
   return(pred_ko(model,test))
 }
@@ -167,8 +167,8 @@ Scenario1Enriched <- function(size,ncov,seed){
   }
   mu <- GetQFunctionValues(X, A)
   R = rnorm(length(mu),mu,1)
-  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu, 
-                  GetQFunctionValues=GetQFunctionValues, 
+  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu,
+                  GetQFunctionValues=GetQFunctionValues,
                   GetOptimalTreatment=GetOptimalTreatment)
   return(datainfo)
 }
@@ -177,7 +177,7 @@ Scenario1Enriched <- function(size,ncov,seed){
 
 Scenario2Enriched <- function(size,ncov,seed){
   GetOptimalTreatment <- function(X) {
-    I(X[,1] > -0.5)*I(X[,1] < 0.5)*0.6 + 1.2*I(X[,1] > 0.5) + 1.2*I(X[,1] < -0.5) + 
+    I(X[,1] > -0.5)*I(X[,1] < 0.5)*0.6 + 1.2*I(X[,1] > 0.5) + 1.2*I(X[,1] < -0.5) +
       X[,4]^2 + 0.5*log(abs(X[,7])+1) - 0.6
   }
   set.seed(seed)
@@ -189,8 +189,8 @@ Scenario2Enriched <- function(size,ncov,seed){
   }
   mu =   GetQFunctionValues(X, A)
   R = rnorm(length(mu),mu,1)
-  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu, 
-                  GetQFunctionValues=GetQFunctionValues, 
+  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu,
+                  GetQFunctionValues=GetQFunctionValues,
                   GetOptimalTreatment=GetOptimalTreatment)
   return(datainfo)
 }
@@ -210,11 +210,11 @@ Scenario4Enriched <- function(size,ncov,seed){
   }
   mu = GetQFunctionValues(X, A)
   R = rnorm(length(mu),mu,1)
-  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu, 
-                  GetQFunctionValues=GetQFunctionValues, 
+  datainfo = list(X=X, A=A, R=R, D_opt=D_opt, mu=mu,
+                  GetQFunctionValues=GetQFunctionValues,
                   GetOptimalTreatment=GetOptimalTreatment)
   return(datainfo)
-  
+
 }
 
 ## Sanity checks
@@ -231,27 +231,27 @@ Scenario4Enriched <- function(size,ncov,seed){
 
 
 
-# HOW TO USE : 
-# 
+# HOW TO USE :
+#
 # n_samples <- 100
 # n_test_samples <- 100
 # noise_sd <- 0
-# 
+#
 # GetData  <- GetDataForShvechikov.2
 # GetData  <- GetDataForShvechikov.1
 # train <- GetData(n_samples, noise_sd)
 # test <- GetData(n_test_samples, noise_sd)
 # ko_train <- ChangeFormatFromOurToChenEnriched(train)
 # ko_test <- ChangeFormatFromOurToChenEnriched(test)
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
 # n_samples <- 100
 # n_test_samples <- 200
 # n_covariates <- 10
-# 
+#
 # GetData <- Scenario1Enriched
 # GetData <- Scenario2Enriched
 # GetData <- Scenario4Enriched
